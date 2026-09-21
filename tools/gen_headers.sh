7350cdc2fe3261c1cbb63baef24689d22d31b8ee
dino <noah.huelskaemper@gmx.de>
2026-09-21 21:32:52 +0200

    Added header

--- tools/gen_headers.sh
+++ tools/gen_headers.sh
@@ -6,7 +6,7 @@ for h in "what-is-this|WHAT IS THIS?" "quick-start|QUICK START" "the-dabt-comman
   "features|FEATURES" "security-scan|SECURITY SCAN" "plugins|PLUGINS" "screenshots|SCREENSHOTS" \
   "themes|THEMES" "documentation|DOCUMENTATION" "project-structure|PROJECT STRUCTURE" \
   "requirements|REQUIREMENTS" "going-beyond-the-demo|GOING BEYOND THE DEMO" "contributing|CONTRIBUTING" \
-  "release-title|D.A.B.T 0.0.8" "release-new|NEW IN 0.0.8" "release-install|INSTALL OR UPDATE" "release-package|PACKAGE YOUR OWN APP" \
+  "release-title|D.A.B.T 0.0.8: BUILD SIGN AND SHIP YOUR APPS" "release-new|WHATS NEW" "release-install|INSTALL OR UPDATE" "release-package|PACKAGE YOUR OWN APP" \
   "release-added|ADDED" "release-changed|CHANGED" "release-fixed|FIXED"; do
   "$d/gen_header.sh" "${h#*|}" "$d/../assets/headers/${h%%|*}.svg" >/dev/null
 done
